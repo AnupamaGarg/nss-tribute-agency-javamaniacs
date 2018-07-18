34c45209d94c8dca1aefad6782dd8f825acdd633
Daniel Brewer <danielbrewer2014@comcast.net>
2018-07-18 14:11:25 -0500

    local storage database

---
+++ extras.js
@@ -0,0 +1,10 @@
+<button type="button" class="btn btn-primary">Official Website</button>
+<button type="button" class="btn btn-secondary"></button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
