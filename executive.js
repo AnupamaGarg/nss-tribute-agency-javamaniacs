d422701161193dabfa95f7145b308459d7fba0d3
cain910772 <cain910772@yahoo.com>
2018-07-18 15:44:30 -0500

    save1

---
+++ executive.js
@@ -0,0 +1,12 @@
+document.getElementById('Career').onclick = function URL() {
+    location.href = 'http:google.com';
+}
+document.getElementById('Personal').onclick = function URL() {
+    location.href = 'http:google.com';}
+document.getElementById('Extras').onclick = function URL() {
+    location.href = 'http:google.com'}
+document.getElementById('Newsfeed').onclick = function URL() {
+    location.href = 'http:google.com';}
+ document.getElementById('Home').onclick = function URL() {
+    location.href = 'http:google.com';
+}
