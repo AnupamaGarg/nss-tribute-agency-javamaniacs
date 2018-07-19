8f4bb8680e3a4e4e0d234b4e7f75d52dfdddf1b4
Daniel Brewer <danielbrewer2014@comcast.net>
2018-07-19 15:13:04 -0500

    added borders and changed text color

--- extras.js
+++ extras.js
@@ -1,19 +1,19 @@
 // inject html for buttons using JavaScript
 
 document.getElementById("Official").innerHTML = `<a href="https://www.paulmccartney.com/">
-<button type="button" class="btn btn-primary">Official Website</button></a>`;
+<button type="button" class="btn off-white">Official Website</button></a>`;
 
 document.getElementById("Wikipedia").innerHTML = `<a href="https://en.wikipedia.org/wiki/Paul_McCartney">
-<button type="button" class="btn btn-secondary">Wikipedia</button></a>`;
+<button type="button" class="btn turquoise">Wikipedia</button></a>`;
 
 document.getElementById("Fan-Forum").innerHTML = `<a href="http://maccaboard.paulmccartney.com//">
-<button type="button" class="btn btn-success">Fan Forum</button></a>`;
+<button type="button" class="btn turquoise">Fan Forum</button></a>`;
 
 document.getElementById("iTunes").innerHTML = `<a href="https://itunes.apple.com/us/artist/paul-mccartney/12224">
-<button type="button" class="btn btn-danger btn-block">iTunes</button></a>`;
+<button type="button" class="btn off-white btn-block border border-dark">iTunes</button></a>`;
 
 document.getElementById("Spotify").innerHTML = `<a href="https://open.spotify.com/artist/4STHEaNw4mPZ2tzheohgXB">
-<button type="button" class="btn btn-warning btn-block">Spotify</button></a>`;
+<button type="button" class="btn tangerine btn-block border border-dark">Spotify</button></a>`;
 
 //  makes div for tour dates
 
@@ -26,7 +26,7 @@ return div;
 
 let tourSchedule = createDiv();
 for (let i = 0; i < TourDatabase.TourDates.length; i++) {
-    tourSchedule.innerHTML += `<p class="d-inline mx-1 border">${TourDatabase.TourDates[i].city} ${TourDatabase.TourDates[i].date}</p>`;
+    tourSchedule.innerHTML += `<p class="d-inline mx-2 border border-dark">${TourDatabase.TourDates[i].city} ${TourDatabase.TourDates[i].date}</p>`;
     document.getElementById("Tour-Schedule").appendChild(tourSchedule);
 }
 
