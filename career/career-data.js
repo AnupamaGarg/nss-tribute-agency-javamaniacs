342852a54c804ae0b64d42de4cf86f0c06045657
Jonathan Edwards <jonathan_edwards@Jonathans-MacBook-Pro.local>
2018-07-18 19:46:52 -0500

    Added more DOM elements via JS

--- career/career-data.js
+++ career/career-data.js
@@ -21,33 +21,41 @@ const introP = {
 //Skill data
 const guitar = {
     skill: "Playing Guitar",
-    description: "Paul has been playing guitar his entire life."
-}
-
-const singing = {
-    skill: "Singing",
-    description: "Mr. McCartney has a mean set of pipes."
+    description: "Paul has been playing guitar his entire life.",
+    img: "img/electric-guitar.svg"
 }
 
 const writing = {
     skill: "Songwriting",
-    description: "He's written millions of great songs."
+    description: "He's written millions of great songs.",
+    img: "img/vinyl.svg"
+}
+
+const singing = {
+    skill: "Singing",
+    description: "Mr. McCartney has a mean set of pipes.",
+    img: "img/microphone.svg"
 }
 
 //Works data
 const quarrymen = {
     bandName: "The Quarrymen",
-    about: "McCartney's first band"
+    about: "The Quarrymen are a British skiffle rock and roll group which eventually evolved into the Beatles in 1960. Originally consisting of Lennon and several schoolfriends, the Quarrymen took their name from a line in the school song of Quarry Bank High School, which they attended. Lennon's mother, Julia Lennon, taught her son to play the banjo and then showed Lennon and Eric Griffiths how to tune their guitars in a similar way to the banjo, and taught them simple chords and songs."
 }
 
 const beatles = {
     bandName: "The Beatles",
-    about: "The biggest rock band in the world"
+    about: "The Beatles were an English rock band formed in Liverpool in 1960. With members John Lennon, Paul McCartney, George Harrison and Ringo Starr, they became widely regarded as the foremost and most influential music band in history Rooted in skiffle, beat and 1950s rock and roll, the Beatles later experimented with several musical styles, ranging from pop ballads and Indian music to psychedelia and hard rock, often incorporating classical elements and unconventional recording techniques in innovative ways."
 }
 
 const wings = {
     bandName: "Wings",
-    about: "Paul's band after The Beatles"
+    about: "Paul McCartney and Wings, also known simply as Wings, were a rock band formed in 1971 by former Beatle Paul McCartney with his wife Linda on keyboards, session drummer Denny Seiwell, and former Moody Blues guitarist Denny Laine. Wings were noted for frequent personnel changes as well as commercial success, going through three lead guitarists and four drummers. However, the core trio of the McCartneys and Laine remained intact throughout the group's existence."
+}
+
+const fireman = {
+    bandName: "The Fireman",
+    about: "The Fireman is the experimental music duo of Paul McCartney and Youth formed in the early 1990s. Their music catalogue ranges from rock to electronica, evolving over more than two decades and three albums. Although officially anonymous until 2008 with the release of the album Electric Arguments,[1] the group members' names had been known to the public since soon after the release of their first album."
 }
 
 //Collaborations
@@ -84,8 +92,8 @@ const award3 = {
 
 //Push data in to the appropriate array
 careerDatabase.intro.push(introP);
-careerDatabase.skills.push(guitar, singing, writing);
-careerDatabase.works.push(quarrymen, beatles, wings);
+careerDatabase.skills.push(guitar, writing, singing);
+careerDatabase.works.push(quarrymen, beatles, wings, fireman);
 careerDatabase.collabs.push(kanye, michael, stevie);
 careerDatabase.awards.push(award1, award2, award3);
 
--- career/career-main.js
+++ career/career-main.js
@@ -16,7 +16,7 @@ const createDiv = () => {
 //Creates the div and stores it in introHeader
 const introHeader = createContainer();
 //Gets value from database and sets it to the value of the introHeader
-introHeader.innerHTML = "<h2>" + careerDatabase.intro[0].title + "</h2>";
+introHeader.innerHTML = `<h2 class = "text-center">${careerDatabase.intro[0].title}</h2>`;
 //Appends the introHeader to the page as a child of the careerContent article
 careerContent.appendChild(introHeader)
 
@@ -25,51 +25,160 @@ introText.innerHTML = careerDatabase.intro[0].content;
 
 introHeader.appendChild(introText);
 
+const skillContainer = createDiv();
+skillContainer.classList.add("bg-secondary", "py-1", "my-4");
+careerContent.appendChild(skillContainer);
+
 //Skills section
+const skillsHeader = createContainer();
+skillsHeader.classList.add("text-light")
+skillsHeader.innerHTML = `<h2>Skills</h2>`;
+
+skillContainer.appendChild(skillsHeader)
+
 const skills = createContainer();
+skills.classList.add("bg-secondary", "my-3");
+skills.innerHTML = `<div class="card-deck border-0 py-1 text-light">
+<div class="card border-0 bg-transparent">
+  <img class="card-img-top icon mx-auto" src=${careerDatabase.skills[0].img} alt="Card image cap">
+  <div class="card-body pb-0">
+    <h5 class="card-title text-center">${careerDatabase.skills[0].skill}</h5>
+    <p class="card-text">${careerDatabase.skills[0].description}</p>
+  </div>
+</div>
+<div class="card border-0 bg-transparent">
+  <img class="card-img-top icon mx-auto" src=${careerDatabase.skills[1].img} alt="Card image cap">
+  <div class="card-body text-center pb-0">
+    <h5 class="card-title">${careerDatabase.skills[1].skill}</h5>
+    <p class="card-text">${careerDatabase.skills[1].description}</p>
+  </div>
+</div>
+<div class="card border-0 text-center bg-transparent">
+  <img class="card-img-top icon mx-auto" src=${careerDatabase.skills[2].img} alt="Card image cap">
+  <div class="card-body pb-0">
+    <h5 class="card-title">${careerDatabase.skills[2].skill}</h5>
+    <p class="card-text">${careerDatabase.skills[2].description}</p>
+  </div>
+</div>
+</div>`
 
-careerContent.appendChild(skills);
 
-const skill1 = createDiv();
-const skill2 = createDiv();
-const skill3 = createDiv();
-
-skill1.innerHTML = "<h3>" + careerDatabase.skills[0].skill + "</h3>" + "<p>" + careerDatabase.skills[0].description + "</p>";
-skills.appendChild(skill1);
-
-skill2.innerHTML = "<h3>" + careerDatabase.skills[1].skill + "</h3>" + "<p>" + careerDatabase.skills[1].description + "</p>";
-skills.appendChild(skill2);
-
-skill3.innerHTML = "<h3>" + careerDatabase.skills[2].skill + "</h3>" + "<p>" + careerDatabase.skills[2].description + "</p>";
-skills.appendChild(skill3);
+skillContainer.appendChild(skills);
 
 
 //Creates the 'works' cards
 const works = createContainer();
-works.innerHTML = `<div class="card-group">
+const worksHeader = createContainer();
+worksHeader.innerHTML = `<h2 class = "text-right">Notable Work</h2>`;
+//Appends works cards to the page
+careerContent.appendChild(worksHeader);
+worksHeader.appendChild(works)
+
+
+
+works.innerHTML = `<div class="card-group shadows my-3">
     <div class="card">
-        <img class="card-img-top" src="https://placeimg.com/251/180/nature" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">${careerDatabase.works[0].bandName}</h5>
                 <p class="card-text">${careerDatabase.works[0].about}</p>
             </div>
-</div>
+    </div>
         <div class="card">
-            <img class="card-img-top" src="https://placeimg.com/251/180/arch" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">${careerDatabase.works[1].bandName}</h5>
-                    <p class="card-text">${careerDatabase.works[1].about}</p>
+                    <p class="card-text">${careerDatabase.works[1].about}</p> 
 
                 </div>
-</div>
+    </div>
             <div class="card">
-                <img class="card-img-top" src="https://placeimg.com/251/180/tech" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title">${careerDatabase.works[2].bandName}</h5>
                         <p class="card-text">${careerDatabase.works[2].about}</p>
                     </div>
-</div>
+    </div>
+        <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${careerDatabase.works[3].bandName}</h5>
+                        <p class="card-text">${careerDatabase.works[3].about}</p>
+                    </div>
+        </div>
             </div>`;
 
-//Appends works cards to the page
-careerContent.appendChild(works);
+
+
+
+
+const cardColumns = createContainer();
+const collabs = createDiv();
+collabs.innerHTML = `<h2 class = "container">Collaborations</h2>`;
+
+careerContent.appendChild(collabs);
+collabs.appendChild(cardColumns);
+
+cardColumns.classList.add("my-3");
+cardColumns.innerHTML = `<div class="card-columns bg-info p-2">
+<div class="card">
+  <img class="card-img-top" src="https://placeimg.com/225/160/animals" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title that wraps to a new line</h5>
+    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+  </div>
+</div>
+<div class="card p-3">
+  <blockquote class="blockquote mb-0 card-body">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+    <footer class="blockquote-footer">
+      <small class="text-muted">
+        Someone famous in <cite title="Source Title">Source Title</cite>
+      </small>
+    </footer>
+  </blockquote>
+</div>
+<div class="card">
+  <img class="card-img-top" src="https://placeimg.com/225/160/nature" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
+<div class="card bg-primary text-white text-center p-3">
+  <blockquote class="blockquote mb-0">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
+    <footer class="blockquote-footer">
+      <small>
+        Someone famous in <cite title="Source Title">Source Title</cite>
+      </small>
+    </footer>
+  </blockquote>
+</div>
+<div class="card text-center">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This card has a regular title and short paragraphy of text below it.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
+<div class="card">
+  <img class="card-img" src="https://placeimg.com/225/260/arch" alt="Card image">
+</div>
+<div class="card p-3 text-right">
+  <blockquote class="blockquote mb-0">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+    <footer class="blockquote-footer">
+      <small class="text-muted">
+        Someone famous in <cite title="Source Title">Source Title</cite>
+      </small>
+    </footer>
+  </blockquote>
+</div>
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
+</div>`
+
+careerContent.appendChild(cardColumns);
